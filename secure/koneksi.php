4c27b917b56fc549d9bf7f18a33448c9c4a1eb2b
Rey Dylanza <reydylanza@gmail.com>
2025-12-10 10:14:34 +0700

    Update koneksi.php

--- secure/koneksi.php
+++ secure/koneksi.php
@@ -1,14 +1,14 @@
 <?php
-// koneksi ke mysqli
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $db = "salut_ut";
 $database = "salut_ut";
-// Create connection
+
 $koneksi = mysqli_connect($servername, $username, $password,$db);
 $link = mysqli_connect($servername, $username, $password, $database);
-// Check connection
+
 if (!$koneksi) {
 die("Connection failed: " . mysqli_connect_error());
 }
